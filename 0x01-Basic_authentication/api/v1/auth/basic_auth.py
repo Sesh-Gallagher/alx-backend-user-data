0f264a337a0d5cdea16893b241b0a7496706cf7c
Sesh-Gallagher <t.ntwana@gmail.com>
2024-11-16 03:05:15 +0000

    basic auth fix

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-"""Basic authentication module for the API.
+"""
+Basic authentication module for the API.
 """
 import re
 import base64
@@ -11,12 +12,14 @@ from models.user import User
 
 
 class BasicAuth(Auth):
-    """Basic authentication class.
+    """
+    Basic auth class.
     """
     def extract_base64_authorization_header(
             self,
             authorization_header: str) -> str:
-        """Extracts the Base64 part of the Authorization header
+        """
+        Extracts Base64 part of the Authorization header
         for a Basic Authentication.
         """
         if type(authorization_header) == str:
@@ -30,7 +33,8 @@ class BasicAuth(Auth):
             self,
             base64_authorization_header: str,
             ) -> str:
-        """Decodes a base64-encoded authorization header.
+        """
+        Decodes base64-encoded authorization header.
         """
         if type(base64_authorization_header) == str:
             try:
@@ -46,7 +50,8 @@ class BasicAuth(Auth):
             self,
             decoded_base64_authorization_header: str,
             ) -> Tuple[str, str]:
-        """Extracts user credentials from a base64-decoded authorization
+        """
+        Extract the user credentials from a base64-decoded authorization
         header that uses the Basic authentication flow.
         """
         if type(decoded_base64_authorization_header) == str:
@@ -65,7 +70,8 @@ class BasicAuth(Auth):
             self,
             user_email: str,
             user_pwd: str) -> TypeVar('User'):
-        """Retrieves a user based on the user's authentication credentials.
+        """
+        Retrieves user based on the user's authentication credentials.
         """
         if type(user_email) == str and type(user_pwd) == str:
             try:
@@ -79,7 +85,8 @@ class BasicAuth(Auth):
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Retrieves the user from a request.
+        """
+        Retrieves user from a request.
         """
         auth_header = self.authorization_header(request)
         b64_auth_token = self.extract_base64_authorization_header(auth_header)
