6e2c0ff22f5322a1657a451a1d5a63cc56303d0a
Sesh-Gallagher <t.ntwana@gmail.com>
2024-11-21 11:45:01 +0000

    User authentication service

---
+++ 0x03-user_authentication_service/user.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Module create a SQLAlchemy model named
+User for a database table named users
+"""
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+
+class User(Base):
+    """
+    Represents table for users
+    """
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    email = Column(String(250), nullable=False)
+    hash_password = Column(String(250), nullable=False)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
