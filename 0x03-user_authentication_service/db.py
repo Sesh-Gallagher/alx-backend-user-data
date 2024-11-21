fc0645529648c95abc3f615fd8407998c14bf476
Sesh-Gallagher <t.ntwana@gmail.com>
2024-11-21 13:39:01 +0000

    SQLAlchemy model named User for a database table named users

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 """ Represents the  Database for ORM """
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, tupe_
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from typing import TypeVar, Type, Mapping, Dict
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm.exc import NoResultFound
 from sqlalchemy.exc import InvalidRequestError
+import bcrypt
 
 from user import Base, User
 
@@ -48,26 +50,27 @@ class DB:
         """
         Module to Find a user based on a set of filters.
         """
-        fields, values = [], []
-        for key, value in kwargs.items():
-            if hasattr(User, key):
-                fields.append(getattr(User, key))
-                values.append(value)
-            else:
-                raise InvalidRequestError()
-        result = self._session.query(User).filter(
-            tuple_(*fields).in_([tuple(values)])
-        ).first()
-        if result is None:
-            raise NoResultFound()
-        return result
+        if not kwargs:
+            raise InvalidRequestError
+
+        column_names = User.__table__.columns.keys()
+        for key in kwargs.keys():
+            if key not in column_names:
+                raise InvalidRequestError
+
+        user = self._session.query(User).filter_by(**kwargs).first()
+
+        if user is None:
+            raise NoResultFound
+
+        return user
 
     def update_user(self, user_id: int, **kwargs) -> None:
         """
         Uses find_user_by to locate a user based on a given id.
         """
-        user = self.find_user_by(id=user_id)
-        if user is None:
+        userz = self.find_user_by(id=user_id)
+        if userz is None:
             return
         update_source = {}
         for key, value in kwargs.items():
