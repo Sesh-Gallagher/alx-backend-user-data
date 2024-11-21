eb9e44dfb6caa00d41b1a25b1b120fddbe48cf20
Sesh-Gallagher <t.ntwana@gmail.com>
2024-11-21 13:10:43 +0000

    create a SQLAlchemy model named User

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -8,31 +8,35 @@ from user import Base, User
 
 
 class DB:
-    """DB class
+    """
+    DB class module
     """
 
     def __init__(self) -> None:
-        """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        Initialize a new DB instance
+        """
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
 
     @property
     def _session(self) -> Session:
-        """Memoized session object
+        """
+        Memoized session object
         """
         if self.__session is None:
             DBSession = sessionmaker(bind=self._engine)
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hash_password: str) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """
-
+        Module that adds a User to db
+        and Returns a User object
         """
-        user = User(email=email, hash_password=hash_password)
-        self._session.add()
+        user = User(email=email, hashed_password=hashed_password)
+        self._session.add(user)
         self._session.commit()
         return user
--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -4,7 +4,7 @@ Module create a SQLAlchemy model named
 User for a database table named users
 """
 from sqlalchemy import Column, Integer, String
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
@@ -13,10 +13,10 @@ class User(Base):
     """
     Represents table for users
     """
-    __tablename__ = "users"
+    __tablename__ = 'users'
 
-    id = Column(Integer, primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hash_password = Column(String(250), nullable=False)
-    session_id = Column(String(250), nullable=True)
-    reset_token = Column(String(250), nullable=True)
+    session_id = Column(String(250))
+    reset_token = Column(String(250))
